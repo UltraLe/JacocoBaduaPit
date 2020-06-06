6be1385bed0e354026f369835da629412dcc9e6f
ezio <iixelite@gmail.com>
2020-06-06 19:05:51 +0200

    Pitest v3 OK->JACOCO + PITEST Add: ba-dua

--- AnotherUselessModule/src/main/java/superPackage/SuperClass.java
+++ AnotherUselessModule/src/main/java/superPackage/SuperClass.java
@@ -3,6 +3,11 @@ package superPackage;
 public class SuperClass
 {
     public String Hey(){
+        boolean a = true;
+        if(a){
+            return "Yeh";
+        }
+
         return "Hey";
     }
 
--- AnotherUselessModule/src/test/java/superPackage/SuperTest.java
+++ AnotherUselessModule/src/test/java/superPackage/SuperTest.java
@@ -2,13 +2,10 @@ package superPackage;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
-
 public class SuperTest
 {
     @Test
     public void WhatATest(){
-        assertThat(new SuperClass().Hey(), containsString("y"));
+        new SuperClass().Hey();
     }
 }
