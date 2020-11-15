e846f2bd5ab1c9702758f3b62750e5773fc72d1f
ezio <iixelite@gmail.com>
2020-11-15 11:30:51 +0100

    Pitest v3 OK->JACOCO + PITEST Add: ba-dua Modified (test 1) .travis.xml

--- AnotherUselessModule/src/test/java/superPackage/SuperTest.java
+++ AnotherUselessModule/src/test/java/superPackage/SuperTest.java
@@ -1,5 +1,6 @@
 package superPackage;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SuperTest
@@ -7,5 +8,13 @@ public class SuperTest
     @Test
     public void WhatATest(){
         new SuperClass().Hey();
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void NotATest()
+    {
+        int a = 2*3243245;
+        Assert.assertTrue(true);
     }
 }
